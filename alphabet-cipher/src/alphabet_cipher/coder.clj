8a296f9ef7c2746b153bc797693b83e898b03b29
Steven Chan <steven.chan@yodle.com>
2015-10-07 16:02:28 -0400

    Initial solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,61 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(defn getIndex [letter]
+  (.indexOf alphabet (str letter))
+  )
+
+(defn convertToIndices [letterSequence]
+  (map getIndex letterSequence)
+  )
+
+(defn lookupLetter [index]
+  (str (nth (cycle alphabet) index))
+  )
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str
+         (map lookupLetter
+              (map +
+                   (convertToIndices (take (count (seq message)) (cycle (seq keyword))))
+                    (convertToIndices (seq message))
+              )
+          )
+     )
+  )
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str
+       (map lookupLetter
+            (map (fn [x] (+ 26 x))
+                 (map -
+                      (convertToIndices (seq message))
+                      (convertToIndices (take (count (seq message)) (cycle (seq keyword))))
+                    )
+              )
+        )
+    )
+  )
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn decypher [cypher message]
+  (def a (atom 1))
+
+  (def decyphered (apply str
+         (map lookupLetter
+              (map (fn [x] (+ 26 x))
+                   (map -
+                        (convertToIndices (seq cypher))
+                        (convertToIndices (seq message))
+                        )
+                   )
+              )
+         )
+    )
+
+  (while (= false (= (take (count (seq decyphered)) (cycle (take @a decyphered))) (seq decyphered)))
+    (do (take a decyphered) (swap! a inc))
+    )
+  (apply str (take @a decyphered))
+  )
 
